378299a208ba42d24082bff0ecb3db9c6542e882
unknown <20ucs183@lnmiit.ac.in>
2025-02-13 02:04:45 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://mamtagalley.onrender.com/' }));
+
 app.use(express.json());
 
 
